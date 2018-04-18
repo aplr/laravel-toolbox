857e6e39818bb0923fcebf9b57e121ea87d88fc5
Andreas Pfurtscheller <andreas.pfurtscheller@gmail.com>
2018-04-18 11:20:04 +0200

    Added uuid trait

---
+++ src/Traits/UuidTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Aplr\Toolbox\Traits;
+
+use Ramsey\Uuid\Uuid;
+
+trait UuidTrait
+{
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
+
+    public static function bootIdentifiableTrait()
+    {
+        self::creating(function ($model) {
+            $keyName = $model->getKeyName();
+
+            if (!isset($model->{$keyName}) || empty($model->{$keyName})) {
+                $model->{$keyName} = Uuid::uuid4()->toString();
+            }
+        });
+    }
+}
