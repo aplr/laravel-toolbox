c5899432337814c119d103a72ea862ddf531ecb8
Andreas Pfurtscheller <andreas.pfurtscheller@gmail.com>
2018-08-08 18:26:00 +0200

    make api a symfony httpexception

--- src/Exceptions/ApiException.php
+++ src/Exceptions/ApiException.php
@@ -2,9 +2,14 @@
 
 namespace Aplr\Toolbox\Exceptions;
 
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
 use Exception;
 
-class ApiException extends Exception
+class ApiException extends HttpException
 {
-
+    public function __construct(string $message, int $code = 0, Exception $previous = null)
+    {
+        parent::__construct(400, $message, $previous, [], 0);
+    }
 }
