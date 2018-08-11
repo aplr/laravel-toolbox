04857e4717e2750d062a925b0832e4856c522be7
Andreas Pfurtscheller <andreas.pfurtscheller@gmail.com>
2018-08-11 20:37:59 +0200

    pass exception code

--- src/Exceptions/ApiException.php
+++ src/Exceptions/ApiException.php
@@ -10,6 +10,6 @@ class ApiException extends HttpException
 {
     public function __construct(string $message, int $code = 0, Exception $previous = null)
     {
-        parent::__construct(400, $message, $previous, [], 0);
+        parent::__construct(400, $message, $previous, [], $code);
     }
 }
